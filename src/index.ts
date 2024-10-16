a735c123bf3a995326629f3c394cbb91bedd8b9c
Jack <jackbaker321@icloud.com>
2024-10-15 20:12:32 -0500

    Base

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+class APIVerifier {
+
+    constructor() {
+
+    }
+
+}
+
+
+export default {
+    
+};
---
+++ testing/index.js
@@ -0,0 +1,4 @@
+// test.js
+import yollama from '../dist/index.js';
+
+console.log(yollama.module_out);
